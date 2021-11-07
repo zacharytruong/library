c59f579d7d2240c0612b3ce00d63f687d696e5c0
Zachary Truong <zacharytruong@gmail.com>
2021-11-07 10:06:23 -0600

    adding modal

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,20 @@
+// Global variables
+const modal = document.getElementById("add-book-form");
+const addBookBtn = document.getElementById("add-book");
+const closeModalBtn = document.querySelector(".close");
+
+// Add book form modal
+addBookBtn.addEventListener("click", () => {
+  modal.style.display = "block";
+})
+closeModalBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+})
+window.addEventListener("click", e => {
+  if (e.target == modal){
+    modal.style.display = "none";
+  }
+})
 let myLibrary = [
   
 ];
